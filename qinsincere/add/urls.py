751c87058daf389b97484113501dbb29629abc22
Bill Liu <bill.liu.school@gmail.com>
2019-01-12 01:36:28 -0800

    finished django tutorial

--- qinsincere/add/urls.py
+++ qinsincere/add/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from . import views
 
+app_name = 'add'
 urlpatterns = [
-    path('', views.index, name='index'),
+    # ex: /add/
+    path('', views.IndexView.as_view(), name='index'),
+    # ex: /add/5/
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    # ex: /add/5/results/
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
 
