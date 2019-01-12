260ad05c27efd5fbfa9b60f592bc9f35d2f528c4
Bill Liu <bill.liu.school@gmail.com>
2019-01-12 11:15:51 -0800

    finalized chrome ext

--- chrnext2/popup.js
+++ chrnext2/popup.js
@@ -2,4 +2,16 @@ chrome.tabs.executeScript( {
     code: "window.getSelection().toString();"
 }, function(selection) {
     document.getElementById("output").innerHTML = selection[0];
+    const Httprq = new XMLHttpRequest();
+    const theurl = "http://localhost:7000/";
+    Httprq.open("POST", theurl, true);
+
+    Httprq.onreadystatechange = function() {
+        if (Httprq.readyState == 4){
+            // Alert response
+            alert(Httprq.responseText);
+        }
+    };
+
+    Httprq.send(selection[0]);
 });
